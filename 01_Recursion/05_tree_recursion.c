3a62e0f84a40f7a7611c23849319ff6805cea17e
mHamzaHanif <hamzaarain1999@gmail.com>
2020-12-04 18:47:27 +0500

    added more files

--- 01_Recursion/05_tree_recursion.c
+++ 01_Recursion/05_tree_recursion.c
@@ -1,17 +1,16 @@
-// Head Recursion
+// Tree Recursion
 
 #include <stdio.h>
 
 void fun(int n){
     if(n>0){
+        printf("%d ",n);
         fun(n-1);
-        printf("%d ",n);    
+        fun(n-1);
     }
 }
 
-
 int main() {
-    int x=3;
-    fun(x);
-    return 0;
+    fun(3);
+    return 0;   
 }
